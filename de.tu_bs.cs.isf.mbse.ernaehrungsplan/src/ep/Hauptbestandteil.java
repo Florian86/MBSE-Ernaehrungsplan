ac2a84cd4238bf7c4e4efe3bd97c286313cf4eb5
Florian86 <florian86@gmail.com>
2015-01-20 14:22:27 +0100

    Zurückgesetzt

---
+++ de.tu_bs.cs.isf.mbse.ernaehrungsplan/src/ep/Hauptbestandteil.java
@@ -0,0 +1,17 @@
+/**
+ */
+package ep;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Hauptbestandteil</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see ep.MetamodelPackage#getHauptbestandteil()
+ * @model
+ * @generated
+ */
+public interface Hauptbestandteil extends Zutat {
+} // Hauptbestandteil
--- de.tu_bs.cs.isf.mbse.ernaehrungsplan/src/transf/ModelLoader.java
+++ de.tu_bs.cs.isf.mbse.ernaehrungsplan/src/transf/ModelLoader.java
@@ -7,21 +7,22 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
-import de.tu_bs.cs.isf.mbse.ernaehrungsplan.metamodel.Ernaehrungsplan;
+import ep.EpElement;
+import ep.resource.ep.mopp.EpResourceFactory;
 
 /*
  * siehe Vogella 3.2
  * (http://www.vogella.com/tutorials/EclipseEMFPersistence/article.html#javacode_save)
  */
 public class ModelLoader {
-    public Ernaehrungsplan loadModel(String folder, String fileName) {
-        //Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ep", new CvResourceFactory());//TODO: EMFText missing
+    public EpElement loadModel(String folder, String fileName) {
+        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ep", new EpResourceFactory());//TODO: EMFText missing
 
         ResourceSet rs = new ResourceSetImpl();
         File orig = new File(folder + File.separator + fileName + ".ep");
         URI uri = URI.createFileURI(orig.getAbsolutePath());
         Resource resource = rs.getResource(uri, true);
-        Ernaehrungsplan model = (Ernaehrungsplan) resource.getContents().get(0);
+        EpElement model = (EpElement) resource.getContents().get(0);
         return model;
     }
 }
