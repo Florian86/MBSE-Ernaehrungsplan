d4d778c92f7cfb351c3478076218f3d17734c0e6
Florian86 <florian86@gmail.com>
2015-01-22 20:59:02 +0100

    Neues Projekt "generator" für M2T angelegt

---
+++ de.tu_bs.cs.isf.mbse.Ernaehrungsplan.generator/src/de/tu_bs/cs/isf/mbse/Ernaehrungsplan/generator/ModelLoader.java
@@ -0,0 +1,28 @@
+package de.tu_bs.cs.isf.mbse.Ernaehrungsplan.generator;
+
+import java.io.File;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+
+import de.tu_bs.cs.isf.mbse.Ernaehrungsplan.*;
+import de.tu_bs.cs.isf.mbse.Ernaehrungsplan.resource.ep.mopp.*;
+
+/*
+ * siehe Vogella 3.2
+ * (http://www.vogella.com/tutorials/EclipseEMFPersistence/article.html#javacode_save)
+ */
+public class ModelLoader {
+    public EpElement loadModel(String folder, String fileName) {
+        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ep", new EpResourceFactory());//TODO: EMFText missing
+
+        ResourceSet rs = new ResourceSetImpl();
+        File orig = new File(folder + File.separator + fileName + ".ep");
+        URI uri = URI.createFileURI(orig.getAbsolutePath());
+        Resource resource = rs.getResource(uri, true);
+        EpElement model = (EpElement) resource.getContents().get(0);
+        return model;
+    }
+}
