6e0a26b9b68d2858cd8bb53fb869b07ae5956218
Florian86 <florian86@gmail.com>
2015-01-23 21:05:56 +0100

    Neue Klasse zur PDF-Generierung
    In dieser Klasse wird der Pfad zu pdflatex angegeben.
    Der ist natürlich nutzerabhängig und muss ggf. angepasst werden!

---
+++ de.tu_bs.cs.isf.mbse.Ernaehrungsplan.generator/src/de/tu_bs/cs/isf/mbse/Ernaehrungsplan/generator/GeneratePDF.java
@@ -0,0 +1,24 @@
+package de.tu_bs.cs.isf.mbse.Ernaehrungsplan.generator;
+
+import java.io.File;
+
+import de.nixosoft.jlr.JLRGenerator;
+
+public class GeneratePDF {
+	
+	public static void latex2PDF(File inputFile, File outputPath, File workingDirectory)
+	{
+		try {
+			File latex = new File("/usr/local/texlive/2013/bin/x86_64-darwin/pdflatex");
+			
+			JLRGenerator pdfGen = new JLRGenerator();
+			// löscht temporäre Dateien
+			pdfGen.deleteTempFiles(true, true, true);
+			// PDF-Dokument generieren
+			pdfGen.generate(latex, inputFile, outputPath, workingDirectory);
+		} catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+}
